bfe416dd1d921210b4fde7aa3529d370668d96ec
Connor Tyrrell <connor@brewgene.com>
2024-09-22 20:49:10 -0400

    Updated with datamodel

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -4,26 +4,13 @@ const schema = a.schema({
   Cellar: a
     .model({
       userId: a.string(),
-      Drinks: a.array(a.model({
-        drinkId: a.string(),
-        brand: a.string(),
-        name: a.string(),
-        bottlingSerie: a.string().optional(),
-        statedAge: a.string().optional(),
-        strength: a.string(),
-        type: a.string(),
-        imageUrl: a.string().optional(),
-        bottles: a.array(a.model({
-          id: a.string(),
-          status: a.string(),
-          size: a.string(),
-          price: a.string(),
-          dateAdded: a.string(),
-        })),
-      })),
-      triedDrinkIds: a.array(a.string()),
+      drinks: a.json().array(),
+      triedDrinkIds: a.string().array(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [
+      allow.publicApiKey().to(['read']),
+      allow.owner(),
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -31,6 +18,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
-  },
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
 });
